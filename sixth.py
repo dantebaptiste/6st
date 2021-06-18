9fe48d0ecd0144321623536e94a0c728de871ca2
Dante Baptiste <dantebaptiste@REVI024.local>
2021-06-18 08:44:20 -0500

    sixth

---
+++ sixth.py
@@ -0,0 +1,64 @@
+
+
+
+def madlib1():
+    animals= raw_input('enter a animal name : ')
+    profession = raw_input('enter a profession name: ')
+    cloth = raw_input('enter a piece of cloth name: ')
+    things = raw_input('enter a thing name: ')
+    name= raw_input('enter a name: ')
+    place = raw_input('enter a place name: ')
+    verb = raw_input('enter a verb in ing form: ')
+    food = raw_input('food name: ')
+    print('say ' + food + ', the photographer said as the camera '
+        'flashed! \n' + name + ' and I had gone to ' + place +' '
+        'to get our photos taken on my birthday. \nThe first photo we '
+        'really wanted was a picture of us dressed as ' + animals + ' '
+        'pretending to be a ' + profession + '\n. when we saw the second'
+        ' photo, it was exactly what I wanted.\n We both looked like '
+        + things + ' wearing ' + cloth + ' and ' + verb + ' --exactly '
+        'what I had in mind')
+
+
+
+def madlib2():
+    adjactive = raw_input('enter adjective : ')
+    color = raw_input('enter a color name : ')
+    thing = raw_input('enter a thing name :')
+    place = raw_input('enter a place name : ')
+    person = raw_input('enter a person name : ')
+    adjactive1 = raw_input('enter a adjactive : ')
+    insect = raw_input('enter a insect name : ')
+    food = raw_input('enter a food name : ')
+    verb = raw_input('enter a verb name : ')
+    print('Last night I dreamed I was a ' + adjactive + ' butterfly'
+        '\n with ' + color + ' splocthes that looked like ' + thing +
+        '\n .I flew to ' + place + ' with my bestfriend and ' + person +
+        '\nwho was a ' + adjactive1 + ' ' + insect + ' .We ate some ' +
+        food + '\n when we got there and then decided to ' + verb +
+        '\n and the dream ended when I said-- lets ' + verb + '.')
+
+
+def madlib3():
+    person = raw_input('enter person name: ')
+    color = raw_input('enter color : ')
+    foods = raw_input('enter food name : ')
+    adjective = raw_input('enter a adjective name: ')
+    thing = raw_input('enter a thing name : ')
+    place = raw_input('enter place : ')
+    verb = raw_input('enter verb : ')
+    adverb = raw_input('enter adverb : ')
+    food = raw_input('enter food name: ')
+    things = raw_input('enter a thing name : ')
+
+    print('Today we picked apple from ' + person + "'s Orchard. I had\n"
+        " no idea there were so many different varieties of apples. I\n"
+        " ate " + color + ' apples straight off the tree that tested \n'
+        'like ' + foods + '. Then there was a ' + adjective + ' apple \n'
+        'that looked like a ' + thing + '.When our bag were full, we \n'
+        'went on a free hay ride to ' + place + ' and back. It ended at\n'
+        ' a hay pile where we got to ' + verb + ' ' + adverb + '. I can\n '
+        'hardly wait to get home and cook with the apples. We are going \n'
+        'to make appple ' + food + ' and ' + things + ' pies!.')
+
+madlib3()
